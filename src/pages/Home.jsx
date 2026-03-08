f94f68c38d731bacaf640cc13276019f8989fde2
MihirKumar <mihirmalik34@gmail.com>
2026-03-08 19:01:28 +0530

    Add tailwind css style for dark and light theme

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      {/* Theme Toggle */}
+
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main Container */}
+
+      {/* Footer */}
+    </div>
+  );
+}
