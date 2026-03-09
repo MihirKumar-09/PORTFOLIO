35705b6332350730dd982f972ec8fa22a1362304
MihirKumar <mihirmalik34@gmail.com>
2026-03-09 17:31:44 +0530

    Complete Hero Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 import ThemeToggle from "../components/ThemeToggle";
 import StarBackground from "@/components/StarBackground";
@@ -13,6 +14,9 @@ export default function Home() {
       <Navbar />
 
       {/* Main Container */}
+      <main>
+        <HeroSection />
+      </main>
 
       {/* Footer */}
     </div>
