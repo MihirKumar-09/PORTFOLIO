941231e2d4885d6eec6fab5cbbe5326999308200
MihirKumar <mihirmalik34@gmail.com>
2026-03-10 00:26:20 +0530

    Complete Porject Section where someone can check my projects on github and on live url

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,3 +1,111 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
+
+const projects = [
+  {
+    id: 1,
+    title: "E-Commerce Web App",
+    description:
+      "A responsive clone of Flipkart built with modern web technologies.",
+    image: "projects/Project1.png",
+    tags: ["React", "MongoDB", "Express"],
+    demoUrl: "#",
+    githubUrl: "#",
+  },
+  {
+    id: 2,
+    title: "AI Chatbot Web App",
+    description:
+      "A ChatGPT-inspired chat application with a clean UI and interactive messaging experience.",
+    image: "/projects/Project2.png",
+    tags: ["React", "MongoDB", "Node.js"],
+    demoUrl: "#",
+    githubUrl: "#",
+  },
+  {
+    id: 3,
+    title: "Property Rental Platform",
+    description:
+      "A vacation rental web app where users can explore and list travel accommodations.",
+    image: "/projects/Project3.png",
+    tags: ["HTML/CSS", "JavaScript", "EJS"],
+    demoUrl: "#",
+    githubUrl: "#",
+  },
+];
+
 export default function ProjectsSection() {
-  return <section></section>;
+  return (
+    <section id="projects" className="py-24 px-4 relative">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          Featured <span className="text-primary"> Projects</span>
+        </h2>
+
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Here are some of my recent projects. Each project was carefully
+          crafted with attention to detail, performance, and user experience
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project, key) => (
+            <div
+              key={key}
+              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+            >
+              <div className="h-48 overflow-hidden">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+              </div>
+
+              <div className="p-6">
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {project.tags.map((tag) => (
+                    <span className="px-2 py-1 text-xs border font-medium rounded-full bg-primary/20 text-secondary-foreground">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
+              <p className="text-muted-foreground text-sm mb-4">
+                {project.description}
+              </p>
+              <div className="flex justify-between items-center">
+                <div className="flex space-x-3">
+                  <a
+                    href={project.demoUrl}
+                    target="_blank"
+                    className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                  >
+                    <ExternalLink size={20} />
+                  </a>
+                  <a
+                    href={project.githubUrl}
+                    target="_blank"
+                    className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                  >
+                    <Github size={20} />
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="text-center mt-12">
+          <a
+            href="https://github.com/MihirKumar-09/"
+            target="_blank"
+            className="cosmic-button w-fit flex items-center mx-auto gap-2"
+          >
+            Check My Github <ArrowRight size={16} />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
