7b8699f60f4e0da5f800348833ffc8d1e80b134a
MihirKumar <mihirmalik34@gmail.com>
2026-03-14 14:47:08 +0530

    Fix footer copy right logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
       <p className="text-sm text-muted-foreground">
-        &copy: {new Date().getFullYear()} Mihir Kumar Malik. All rights
+        &copy; {new Date().getFullYear()} Mihir Kumar Malik. All rights
         reserved.
       </p>
 
